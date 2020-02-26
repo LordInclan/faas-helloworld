e43f851e0109725dbe8d05c5910d12c7081af012
Lord Inclán <35239780+LordInclan@users.noreply.github.com>
2020-02-26 22:59:58 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+from waitress import serve
+
+app = Flask(__name__)
+@app.route("/helloworld", methods=["GET"])
+def service():
+
+    # Mostrar el resultado
+    return "Hola mundo!"
+
+
+serve(app, host="0.0.0.0", port=5000)
